21ddee76e3f3ce8fc54bd1423b3b2b27faae20f6
Selva02092005 <152613512+Selva02092005@users.noreply.github.com>
2026-06-24 15:48:35 +0530

    Add files via upload

---
+++ Hollowpyramid.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Hollowpyramid{
+	public static void main(String[] args){
+		int rows=5;
+		for(int i=0;i<rows;i++){
+			for(int j=0;j<rows-i-1;j++){
+				System.out.print(" ");
+			}
+			for(int k=0;k<2*i+1;k++){
+				if(k==0 || k==2*i || i==rows-1){
+					System.out.print("*");
+				}
+				else{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
